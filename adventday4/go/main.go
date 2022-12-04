793e76aeaa79c8b261b9169d47bf766d405a1764
sliptripfalldown <sliptripfalldown@gmail.com>
2022-12-04 14:04:51 -0600

    day 4

--- adventday4/go/main.go
+++ adventday4/go/main.go
@@ -14,16 +14,13 @@ func main() {
 	lines := strings.Split(string(coordinates), "\n")
 
 	part1Result := part1(lines)
+	part2Result := part2(lines)
 
-	fmt.Printf("part 1 result: %v", part1Result)
+	fmt.Printf("\npart 1 result: %v\n", part1Result)
+	fmt.Printf("part 2 result: %v\n", part2Result)
 }
 
 func part1(lines []string) int {
-
-	return calculateOverlap(lines)
-}
-
-func calculateOverlap(lines []string) int {
 	var overlappingWork int
 
 	for _, line := range lines {
@@ -32,7 +29,24 @@ func calculateOverlap(lines []string) int {
 		left := makeRange(elf[0])
 		right := makeRange(elf[1])
 
-		if deepContains(left, right) {
+		if containsEither(left, right) {
+			overlappingWork++
+		}
+	}
+
+	return overlappingWork
+}
+
+func part2(lines []string) int {
+	var overlappingWork int
+
+	for _, line := range lines {
+		elf := strings.Split(line, ",")
+
+		left := makeRange(elf[0])
+		right := makeRange(elf[1])
+
+		if containsAny(left, right) {
 			overlappingWork++
 		}
 	}
@@ -53,7 +67,7 @@ func makeRange(elf string) []int {
 	return a
 }
 
-func deepContains(left, right []int) bool {
+func containsEither(left, right []int) bool {
 	leftResult := true
 	rightResult := true
 	for _, leftValue := range left {
@@ -67,5 +81,21 @@ func deepContains(left, right []int) bool {
 			rightResult = false
 		}
 	}
+
 	return leftResult || rightResult
+}
+
+func containsAny(left, right []int) bool {
+	for _, leftValue := range left {
+		if slices.Contains(right, leftValue) {
+			return true
+		}
+	}
+	for _, rightValue := range right {
+		if slices.Contains(left, rightValue) {
+			return true
+		}
+	}
+
+	return false
 }
